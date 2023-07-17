6f85100fecd09c78d5cad4b44f562d2f8f0ab70f
Gabriel <gabrielm1603@gmail.com>
2023-07-17 20:53:38 +0700

    first commit

---
+++ page/home/index.php
@@ -0,0 +1,12 @@
+<section class="hero is-medium is-primary is-bold">
+    <div class="hero-body">
+        <div class="container">
+            <h1 class="title animate__animated animate__zoomIn">
+                Sistem Pendukung Keputusan Menggukanan Metode Weighting Product
+            </h1>
+            <h2 class="subtitle animate__animated animate__slideInUp">
+                Tenkologi Rekayasa Perangkat Lunak | Teknologi Informasi Politeknik Negeri Padang
+            </h2>
+        </div>
+    </div>
+</section>
